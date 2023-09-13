891326d6e3b53d27cf7f787fbfb1256437b27e9f
Ritvik5 <sharma.ritvik56@gmail.com>
2023-09-13 12:23:44 +0530

    [Ritvik]Add: Adding Ticketing Microservices and TicketConsumer Microservices to implement RabbitMQ.

---
+++ TicketConsumer Microservices/Consumer/TicketConsumer.cs
@@ -0,0 +1,17 @@
+﻿using MassTransit;
+using SharedModel.Ticket;
+using System.Threading.Tasks;
+
+namespace TicketConsumer_Microservices.Consumer
+{
+    public class TicketConsumer : IConsumer<Ticket>
+    {
+        public async Task Consume(ConsumeContext<Ticket> context)
+        {
+            var data = context.Message;
+            //Validate the Ticket Data
+            //Store to Database
+            //Notify the user via Email / SMS
+        }
+    }
+}
---
+++ Ticketing Microservices/Controllers/TicketController.cs
@@ -0,0 +1,41 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using SharedModel.Ticket;
+using System;
+using System.Threading.Tasks;
+
+namespace Ticketing_Microservices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : Controller
+    {
+        private readonly IBus _bus;
+        public TicketController(IBus bus)
+        {
+            _bus = bus;
+        }
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateTicket(Ticket ticket)
+        {
+            try
+            {
+                if(ticket != null)
+                {
+                    ticket.BookedOn = DateTime.Now;
+                    Uri uri = new Uri("rabbitmq://localhost/ticketQueue");
+                    var endPoint = await _bus.GetSendEndpoint(uri);
+                    await endPoint.Send(ticket);
+                    return Ok();
+                }
+                return BadRequest();
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
